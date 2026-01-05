b5e4997d3a7be33fd9e05fa0f7a7e2a750b08e78
Akifumi NAKAMURA <tmpz84@gmail.com>
2026-01-05 16:59:09 +0900

    Refactor `HomeController` to support advanced search functionality with toggleable input modes and update templates for improved search experience.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\ManifestationRepository;
+use App\Service\ManifestationSearchQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request,  ManifestationRepository $manifestationRepository): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        $searchQuery = ManifestationSearchQuery::fromRequest($request->query->all());
+        $manifestations = $manifestationRepository->searchByQuery($searchQuery);
+
+        return $this->render('manifestation/index.html.twig', [
+            'manifestations' => $manifestations,
+            'search_params' => $request->query->all(),
         ]);
+
     }
 }
